581decd1f1b59df67e53e7a5fb264407684afb61
bola <bola.ringley.co.uk>
2018-03-14 15:06:48 +0000

    added smooth scroll and change header styling when scroll

---
+++ js/main.js
@@ -0,0 +1,56 @@
+//create helper function to use _$ to select element
+
+ const _$ = (element) => {
+ 	return(document.querySelector(element));
+ }
+
+ const _bytagName = (tag) => {
+ 	return (document.getElementsByTagName(tag));
+ }
+
+/***********************************************************************************/
+/*		Smoothly scroll to different pages 
+/************************************************************************************/
+let smoothScroll = (element) => {
+
+	$("body , html").animate({
+		scrollTop: $(element).offset().top - 100
+	} , 1000)
+	$("#hamburger").prop("checked" ,false);
+}
+
+$("#scrollTohome").click(() => {
+		smoothScroll("#heroArea");
+});
+
+$("#scrollToresume").click(() => {
+		smoothScroll("#resume");
+});
+
+$("#scrollToproject").click(() => {
+		smoothScroll("#project");
+		
+});
+
+$("#scrollTocontact").click(() => {
+		smoothScroll("#contact");
+});
+
+/***********************************************************************************/
+/*		Changing header styling
+/************************************************************************************/
+
+let transparentHeader = () => {
+		if ($("body").scrollTop() >= 500) {
+			$("#fixed-header").css("background-color" , "rgba(0,0,0,.8)");
+			$("#fixed-header").css("border-bottom" , "5px solid #000");
+	} else if ($("body").scrollTop() < 500){
+			$("#fixed-header").css("background-color" , "transparent");
+			$("#fixed-header").css("border-bottom" , "none");
+	}
+}
+
+$( window ).scroll(() => {
+	transparentHeader();
+});
+
